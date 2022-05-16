cf23874f0dcbe3dffd2798367e56eb66db880092
Hunter Price <hunterprice4044@gmail.com>
2022-05-15 20:28:55 -0400

    finished setting up software dev project repo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Software-Development-Projects
+# Software Development Projects
 This repo contains submodules pointing to my software development projects for various courses at the University Of Tennessee.
+
+The following are links to the individual projects:
+- [InfiniBuilder](https://github.com/andreicozma1/InfiniBuilder)
+- [LeafNote](https://github.com/andreicozma1/LeafNote)
+- [STEM](https://github.com/andreicozma1/STEM)
+- [Smart-Ledger](https://github.com/CS401-Team-Project/Smart-Ledger)
---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SHOULD_PUSH=false
+if [[ "$1" == "--push" ]]; then
+    SHOULD_PUSH=true
+fi
+
+echo '##############################################################################################'
+echo 'Updating README...'
+echo "# Software Development Projects" > README.md
+echo "This repo contains submodules pointing to my software development projects for various courses at the University Of Tennessee." >> README.md
+echo "">> README.md
+echo "The following are links to the individual projects:" >> README.md
+
+echo '##############################################################################################'
+echo 'Updating submodules...'
+git pull
+git submodule update --init --recursive
+path=$(pwd)
+cat .gitmodules | grep "path" | awk '{print $3}' | while read line
+do
+    cd $line
+
+    echo '##############################################################################################'
+    echo "Pulling newest commits for: $line"
+    git pull
+
+    echo "Updating README.md"
+    url=`git config --get remote.origin.url | sed 's/git@github\.com:/https:\/\/github\.com\//' | sed 's/\.git//'`
+    echo "URL: $url"
+    echo "- [$line]($url)" >> $path/README.md
+    cd $path
+done
+echo '##############################################################################################'
+
+if [[ $SHOULD_PUSH == true ]]; then
+    git add README.md
+    git commit -m "Update README.md"
+    git push
+    echo '##############################################################################################'
+fi
